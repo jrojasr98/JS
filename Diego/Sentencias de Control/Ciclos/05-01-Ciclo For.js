cd4ad1a0f324991e37678d74172772d499ba2291
Juan Diego RR <jrojasr.1998@gmail.com>
2023-08-27 22:55:10 -0600

    Ciclos JavaScript
    Sentencias While, Do While y For

---
+++ Diego/Sentencias de Control/Ciclos/05-01-Ciclo For.js
@@ -0,0 +1,6 @@
+
+
+for(let contador = 0; contador < 5; contador++){
+    console.log(contador);
+}
+console.log("Fin del Ciclo For");
---
+++ Diego/Sentencias de Control/Ciclos/05-02-Ciclo Do While.js
@@ -0,0 +1,8 @@
+let contador = 0;
+
+do{
+    console.log(contador);
+    contador++;
+}while(contador < 5);
+console.log("Fin del ciclo Do While");
+
