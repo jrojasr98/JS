089b608eab6df0a4f9cc9f00caff0b1281bc346b
Juan Diego RR <jrojasr.1998@gmail.com>
2023-08-27 22:26:52 -0600

    Estructura Switch
    Estructura del condicional Switch

---
+++ Diego/Sentencias de Control/Sentencia Switch/Estructura Swtich.js
@@ -0,0 +1,23 @@
+let numero = 7;
+
+let numeroTexto = "Valor desconocido";
+
+switch (numero) {
+  case 1:
+    numeroTexto = "Numero uno";
+    break;
+    case 2:
+    numeroTexto = "Numero dos";
+    break;
+    case 3: 
+    numeroTexto = "Numero tres";
+    break;
+    case 4: 
+    numeroTexto = "Numero cuatro";
+    break;
+
+    default:
+        numeroTexto
+}
+
+console.log(numeroTexto);
